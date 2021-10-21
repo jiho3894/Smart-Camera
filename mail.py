e926d4c026cb0679db260e77d91d569b49bb5081
Charley1013 <79081800+jiho3894@users.noreply.github.com>
2021-10-21 22:02:36 +0900

    Add files via upload

--- mail.py
+++ mail.py
@@ -4,17 +4,22 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-fromEmail = '보내는 사람 이메일 입력'
+# * 이메일 전송 코드 *
+
+# 수신자와 발신자 지정
+fromEmail = 'crsn1111@gmail.com'
 fromEmailPassword = '보내는 사람 이메일의 앱 비밀번호'
-toEmail = '받는 사람 이메일'
+toEmail = 'crsn1013@gmail.com'
 
 def sendEmail(image):
+	# 메일 수신 제목,수발신자,내용
 	msgRoot = MIMEMultipart('related')
-	msgRoot['Subject'] = '인물을 감지했습니다!!'
+	msgRoot['Subject'] = '인물이 감지 되었습니다.'
 	msgRoot['From'] = fromEmail
 	msgRoot['To'] = toEmail
+	msgRoot = MIMEText('현 시간에 인물이 감지 되어 인물 사진을 전송 하셨습니다.')
 	msgRoot.preamble = 'Raspberry pi security camera update'
-
+	
 	msgAlternative = MIMEMultipart('alternative')
 	msgRoot.attach(msgAlternative)
 	msgText = MIMEText('Smart security cam found object')
@@ -27,6 +32,7 @@ def sendEmail(image):
 	msgImage.add_header('Content-ID', '<image1>')
 	msgRoot.attach(msgImage)
 
+	# 지메일은 포트번호 587 사용
 	smtp = smtplib.SMTP('smtp.gmail.com', 587)
 	smtp.starttls()
 	smtp.login(fromEmail, fromEmailPassword)
